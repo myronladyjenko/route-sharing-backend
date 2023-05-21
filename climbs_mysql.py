06e5cbc22b8e858a55ccbf79c9cf84a9528e54f1
ericbuys <eric.buys02@gmail.com>
2023-05-20 21:36:50 -0400

    Added functionality for loading climb data

--- climbs_mysql.py
+++ climbs_mysql.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import ClimbWrapper
 
 class Database:
     def __init__(self):
@@ -10,7 +11,6 @@ class Database:
         )
 
         self.cursor = self.connection.cursor()
-        self.lastID = 'SELECT LAST_INSERT_ID();'
     
     def __del__(self):
         self.cursor.close()
@@ -18,46 +18,67 @@ class Database:
 
     def __setitem__(self, table, values):
         questionMarkStr = ", ".join(('?',)*len(values)) 
-        insertStr = f"INSERT OR IGNORE INTO {table} VALUES (" + questionMarkStr + ")";
+        insertStr = f"INSERT OR IGNORE INTO {table} VALUES (" + questionMarkStr + ");";
 
         self.cursor.execute(insertStr, values)
 
     #Create Tables
     def createTables(self):
         climbs = """CREATE TABLE IF NOT EXISTS Climbs (
-                NAME        TEXT        NOT NULL,
-                WIDTH       INT         NOT NULL,
-                HEIGHT      INT         NOT NULL,
-                ANGLE       INT         NOT NULL,
-                DIFFICULTY  TEXT        NOT NULL,
-                AUTHOR      TEXT        NOT NULL,
-                REGION      TEXT        NOT NULL,
-                HOLDS_SET   TEXT        NOT NULL,
-                HOLD_LIST   INT         NOT NULL AUTO_INCREMENT,
-                PRIMARY KEY (HOLD_LIST)
-            );"""
+                        NAME        TEXT        NOT NULL,
+                        WIDTH       INT         NOT NULL,
+                        HEIGHT      INT         NOT NULL,
+                        ANGLE       INT         NOT NULL,
+                        DIFFICULTY  TEXT        NOT NULL,
+                        AUTHOR      TEXT        NOT NULL,
+                        REGION      TEXT        NOT NULL,
+                        HOLDS_SET   TEXT        NOT NULL,
+                        CLIMB_ID    INT         NOT NULL AUTO_INCREMENT
+                        PRIMARY KEY (CLIMB_ID)
+                    );"""
 
-        holds = """CREATE TABLE IF NOT EXISTS Holds (
-                    HOLD_ID         INT         NOT NULL,
-                    X               INT         NOT NULL,
-                    Y               INT         NOT NULL,
-                    ROT             INT         NOT NULL,
-                    HOLD_LIST       INT         NOT NULL,
-                    PRIMARY KEY (HOLD_LIST),
-                    FOREIGN KEY (HOLD_LIST) REFERENCES Climbs (HOLD_LIST)
-                );"""
+        holds = """ CREATE TABLE IF NOT EXISTS Holds (
+                        HOLD_ID         INT         NOT NULL,
+                        X               INT         NOT NULL,
+                        Y               INT         NOT NULL,
+                        ROT             INT         NOT NULL,
+                        CLIMB_ID        INT         NOT NULL,
+                        PRIMARY KEY (CLIMB_ID),
+                        FOREIGN KEY (CLIMB_ID) REFERENCES Climbs (CLIMB_ID)
+                    );"""
         
         self.cursor.execute(climbs);
         self.cursor.execute(holds)
         self.connection.commit()
 
     #Add a new climb to the db
-    def createClimb(self, climb):
+    def createClimb(self, climbData):
+        climb = ClimbWrapper.Climb(climbData)
+
         self['Climbs'] = (climb.name, climb.width, climb.height, climb.angle, climb.difficulty, climb.author, \
                             climb.region, climb.set, None)
-        
-        holdListID = self.cursor.execute(self.lastID)
+
+        climbID = self.cursor.execute(self.lastID)
 
         for hold in climb.holds:
-            self['Holds'] = (hold.holdID, hold.x, hold.y, hold.rot, holdListID)
+            self['Holds'] = (hold.holdID, hold.x, hold.y, hold.rot, climbID)
 
+    #Retrieves all the climb previews for di
+    def loadClimbPreviews(self):
+        climbPreviewQuery = 'SELECT * FROM Climbs;'
+        climbPreviews = self.cursor.execute(climbPreviewQuery)
+        return climbPreviews.fetchall()
+
+    #Retrieves the full climb data for when the user purchased a climb
+    def loadFullClimb(self, climbID):
+        climbQuery = 'SELECT * FROM Climbs WHERE CLIMB_ID = ?;'
+        holdsQuery = 'SELECT * FROM Holds WHERE CLIMB_ID = ?;'
+
+        climbInfo = self.cursor.execute(climbQuery, (climbID,))
+        climbInfo = climbInfo.fetchone()
+
+        holdsInfo = self.cursor.execute(holdsQuery, (climbID,))
+        holdsInfo = holdsInfo.fetchall()
+
+        climb = ClimbWrapper(climbInfo, holdsInfo)
+        return climb
