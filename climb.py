548095a479dcdce03f616e543c9853d32450d634
ericbuys <eric.buys02@gmail.com>
2023-05-21 01:44:45 -0400

    Created Climb Wrapper Class for new climb input from frontend

--- climb.py
+++ climb.py
@@ -1,9 +1,21 @@
 
 #Class for representing a climbs features instead of a JSON file
 class Climb:
-    def __init__(self, JSONfile):
-        pass
+    def __init__(self, dict):
+        self.name = dict['name']
+        self.width = dict['width']
+        self.height = dict['height']
+        self.angle = dict['angle']
+        self.difficulty = dict['difficulty']
+        self.author = dict['author']
+        self.region = dict['region'] 
+        self.hold_theme = dict['hold_theme']
+        self.hold = []
 
+        for item in dict['holds']:
+            newHold = Hold(item['id'], item['x'], item['y'], item['rotation'])
+            self.hold.append(newHold)
+            
 #Class for representing hold specific information
 class Hold:
     def __init__(self, holdID, x, y, rot):
