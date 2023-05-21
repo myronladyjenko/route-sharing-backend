0af4f2c4f6063d20f2bd937d690082240e22fc68
Myron Ladyjenko <mladyjen@uoguelph.ca>
2023-05-20 21:49:51 -0400

    Update with posting transaction to etherium

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import sys;
 import json;
 from http.server import HTTPServer, BaseHTTPRequestHandler;
+from web3 import web3
 
 climbConfigs = {
   "crimp": [
@@ -30,7 +31,33 @@ class MyHandler( BaseHTTPRequestHandler ):
         self.send_response( 404 );
         self.end_headers();
         self.wfile.write( bytes( "404: not found", "utf-8" ) );
+    
+    def do_POST(self):
+      content_length = int(self.headers['Content-Length'])
+      request_body = self.rfile.read(content_length)
+      purchaseRequest = json.loads(request_body.decode('utf-8'))
+
+      climb_data = purchaseRequest.get('climbData');
+      signed_transaction = purchaseRequest.get('signedTransaction')
+
+      if not signed_transaction:
+        self.send_response(400)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        response = {'error': 'Invalid purchase request'}
+        self.wfile.write(json.dumps(response).encode('utf-8'))
+        return
+
+      transaction_hash = web3.eth.sendRawTransaction(signed_transaction.rawTransaction).hex()
+
+      # potentiall here we need to call Eric's method to update the database
+
+      self.send_response(200)
+      self.send_header('Content-Type', 'application/json')
+      self.end_headers()
+      response = {'message': 'Purchase successful', 'transactionHash': transaction_hash}
+      self.wfile.write(json.dumps(response).encode('utf-8'))
 
 
-httpd = HTTPServer( ( '0.0.0.0', int(sys.argv[1]) ), MyHandler );
+httpd = HTTPServer(('0.0.0.0', int(sys.argv[1])), MyHandler);
 httpd.serve_forever();
